6665747f31d755d59079bda96d53b6b321bb4306
oke <okeokorefe6@gmail.com>
2026-03-26 12:22:51 +0000

    phase 1: SwiftPay Web Interface and Project Foundation

---
+++ src/test/java/com/swiftpay/SwiftpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swiftpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
